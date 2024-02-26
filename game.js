5a1959c0c1ad6bb67125448afe3eacb44f34d6ab
Haider <161382569+21139hf@users.noreply.github.com>
2024-02-27 10:51:01 +1300

    Update game.js
    movement completed

--- game.js
+++ game.js
@@ -5,7 +5,7 @@
 /*******************************************************/
 console.log("%c t01_create_sprite", "color: blue;");
 
-const SCREEN_WIDTH = 500;
+const SCREEN_WIDTH = 500; 
 const SCREEN_HEIGHT = 250;
 const PLAYER_WIDTH = 25;
 const PLAYER_HEIGHT = 25;
@@ -15,28 +15,25 @@ const PLAYER_HEIGHT = 25;
 function setup() {
   console.log("setup: ");
   cnv = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
-  world.gravity.y = 10;
+  world.gravity.y = 20;
+
 
 player = new Sprite(PLAYER_WIDTH*1.2, SCREEN_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'd');
 player.color = 'yellow';
 
 platform_1 = new Sprite(SCREEN_WIDTH/2, SCREEN_HEIGHT, SCREEN_WIDTH, 5, 'k');
+
+      document.addEventListener("keydown", 
+        function(event) {
+            console.log("Key pressed!"+player.y);
+            if(player.y > 224 ){// 224 - found from testing - floor level
+            console.log("Key pressed!");
+            player.vel.y = -10;
+            }
+    });
 }
-  document.addEventListener("keydown", function(event) {
 
-  if (event.code === 'ArrowUp') {
-player.vel.y = -50;
-    // Set sprite's velocity to the up
-  }
-  });
-  
-  document.addEventListener("keyup", function(event) {
-
-  if (event.code === 'ArrowUp') {
-player.vel.y = 0;
-    // Set sprite's velocity to the 0
-  }
-  });
+   
 /*******************************************************/
 // draw()
 /*******************************************************/
