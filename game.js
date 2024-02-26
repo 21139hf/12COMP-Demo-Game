ee3df1c93626e9204247ccf50f20cfd0d627e008
Haider <161382569+21139hf@users.noreply.github.com>
2024-02-27 10:21:52 +1300

    Update game.js
    initial commit

--- game.js
+++ game.js
@@ -1,1 +1,49 @@
+/*******************************************************/
+// P5.play: t01_create_sprite
+// Create a sprite
+// Written by ??? 
+/*******************************************************/
+console.log("%c t01_create_sprite", "color: blue;");
 
+const SCREEN_WIDTH = 500;
+const SCREEN_HEIGHT = 250;
+const PLAYER_WIDTH = 25;
+const PLAYER_HEIGHT = 25;
+/*******************************************************/
+// setup()
+/*******************************************************/
+function setup() {
+  console.log("setup: ");
+  cnv = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+  world.gravity.y = 10;
+
+player = new Sprite(PLAYER_WIDTH*1.2, SCREEN_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'd');
+player.color = 'yellow';
+
+platform_1 = new Sprite(SCREEN_WIDTH/2, SCREEN_HEIGHT, SCREEN_WIDTH, 5, 'k');
+}
+  document.addEventListener("keydown", function(event) {
+
+  if (event.code === 'ArrowUp') {
+player.vel.y = -50;
+    // Set sprite's velocity to the up
+  }
+  });
+  
+  document.addEventListener("keyup", function(event) {
+
+  if (event.code === 'ArrowUp') {
+player.vel.y = 0;
+    // Set sprite's velocity to the 0
+  }
+  });
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+  background('grey');
+}
+
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
